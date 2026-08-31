1deceb216b952d116c54f2e01622515dccdf9e9d
Shrinit <shrinitjichkar7@gmail.com>
2026-08-31 15:55:31 +0530

    updated jwt access token expiration limit to 365 days prevented token expiration during local testing and development

--- fleetlogcore/fleetlogcore/settings.py
+++ fleetlogcore/fleetlogcore/settings.py
@@ -166,7 +166,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=365),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': False,
 }
