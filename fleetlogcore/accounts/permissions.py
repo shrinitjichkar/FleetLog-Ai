3cd23360c290924da7e7fe441bf2c4fa41398968
Shrinit <shrinitjichkar7@gmail.com>
2026-09-11 12:11:57 +0530

    .

--- fleetlogcore/accounts/permissions.py
+++ fleetlogcore/accounts/permissions.py
@@ -43,6 +43,7 @@ class IsAssignedAssessorOrSupervisor(BasePermission):
         )
 
     def has_object_permission(self, request, view, obj):
-        if request.user.role in  ['supervisor','assessor']:
-            return True
-        return obj.assessor_id == request.user.id
+      if request.user.role == 'supervisor':
+       return True
+
+      return obj.assessor_id == request.user.id
